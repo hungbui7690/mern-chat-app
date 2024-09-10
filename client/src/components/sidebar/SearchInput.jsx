b7548b494203ad126bae69ee513a8afd1a06ec88
Hung Bui <hungbui7690@gmail.com>
2024-09-10 13:20:35 -0600

    Search for Conversation

--- client/src/components/sidebar/SearchInput.jsx
+++ client/src/components/sidebar/SearchInput.jsx
@@ -1,17 +1,40 @@
+import { useState } from 'react'
 import { IoSearchSharp } from 'react-icons/io5'
+import useStore from '../../zustand/useStore'
+import useGetConversations from '../../hooks/useGetConversations'
+import toast from 'react-hot-toast'
 
 const SearchInput = () => {
+  const [search, setSearch] = useState('')
+  const { setSelectedConversation } = useStore()
+  const { conversations } = useGetConversations()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!search) return
+    if (search.length < 3) {
+      return toast.error('Search term must be at least 3 characters long')
+    }
+
+    const conversation = conversations.find((c) =>
+      c.fullName.toLowerCase().includes(search.toLowerCase())
+    )
+
+    if (conversation) {
+      setSelectedConversation(conversation)
+      setSearch('')
+    } else toast.error('No such user found!')
+  }
   return (
-    <form className='flex items-center gap-2'>
+    <form onSubmit={handleSubmit} className='flex items-center gap-2'>
       <input
         type='text'
         placeholder='Search…'
         className='input-bordered rounded-full input'
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
-      <button
-        type='submit'
-        className='bg-emerald-500 text-white btn btn-circle'
-      >
+      <button type='submit' className='bg-sky-500 text-white btn btn-circle'>
         <IoSearchSharp className='w-6 h-6 outline-none' />
       </button>
     </form>
