5b3bb99935e1f69483bf09b98f685220c0b22724
Hung Bui <hungbui7690@gmail.com>
2024-09-10 18:09:33 -0600

    Fix Onrender build command

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import SignUp from './pages/signup/SignUp'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import { useAuthContext } from './context/AuthContext'
+import { useAuthContext } from './context/authContext'
 
 function App() {
   const { authUser } = useAuthContext()
