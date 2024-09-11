b3a9f75e0adc91c2ced287d3a72bb62c819160fd
Hung Bui <hungbui7690@gmail.com>
2024-09-10 21:42:17 -0600

    change fetching URL from localhost to PROD URL

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const axiosInstance = axios.create({
   // baseURL: 'http://localhost:5000/api/v1',
-  baseURL: 'https://mern-chat-app-z7fx.onrender.com',
+  baseURL: 'https://mern-chat-app-z7fx.onrender.com/api/v1',
 })
 
 axiosInstance.defaults.withCredentials = true
